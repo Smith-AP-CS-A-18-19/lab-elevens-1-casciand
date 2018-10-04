adaeb5c13dca64fc84a1396fb5e74b284f788441
casciand <casciand@berkeleyprep.org>
2018-10-04 09:16:35 -0400

    elevens 1

--- CardTester.java
+++ CardTester.java
@@ -9,5 +9,19 @@ public class CardTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card a = new Card("king", "hearts", 13);
+		System.out.println(a.suit());
+		System.out.println(a.rank());
+		System.out.println(a.pointValue());
+		System.out.println(a.toString());
+		Card b = new Card("ace", "spades", 1);
+		System.out.println(b.suit());
+		System.out.println(b.rank());
+		System.out.println(b.pointValue());
+		System.out.println(b.toString());
+		Card c = new Card("ace", "spades", 1);
+		System.out.println(a.matches(b));
+		System.out.println(b.matches(c));
+
 	}
 }
